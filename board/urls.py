396816727994882f81295d3de63d0fa576fa23ee
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-31 23:21:04 +0900

    feat	: 운영자 게시판 작성 & 조회 API - 권한제한
    - User의 authorization == 2 경우만 작성 조회 가능

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,6 @@ from .views import NoticePostView, NoticeView
 urlpatterns = [
     path('/notice/post', NoticePostView.as_view()),
     path('/notice/<int:notice_id>', NoticeView.as_view()),
+    path('/adminboard/post', NoticePostView.as_view()),
+    path('/adminboard/<int:adminboard_id>', NoticeView.as_view()),
 ]
--- board/views.py
+++ board/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from eboard.utils import login_decorator
 
 from users.models import User
-from .models import Notice
+from .models import AdminBoard, Notice
 
 
 class NoticePostView(View):
@@ -14,7 +14,6 @@ class NoticePostView(View):
     def post(self, request):
         try:
             user = request.user
-            print(user.id)
 
             if not user.authorization == 2:
                 return JsonResponse({'Message': 'unauthorized'}, status = 401)
@@ -24,7 +23,7 @@ class NoticePostView(View):
             content = data['content']
 
             Notice.objects.create(
-                user_id    = user.id,
+                user_id = user.id,
                 title   = title,
                 content = content
             )
@@ -38,11 +37,55 @@ class NoticeView(View):
         try:
             notice = Notice.objects.get(id = notice_id)
             notice_detail = {
-                'title': notice.title,
-                'writer': notice.user.name,
+                'title'  : notice.title,
+                'writer' : notice.user.name,
                 'content': notice.content
             }
-            return JsonResponse({'results': notice_detail}, status = 200)
 
+            return JsonResponse({'results': notice_detail}, status = 200)
         except Notice.DoesNotExist:
-            return JsonResponse({'Message': 'DOES_NOT_EXIST'}, status = 400)            
+            return JsonResponse({'Message': 'DOES_NOT_EXIST'}, status = 400)
+
+
+class AdminBoardPostView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            user = request.user
+
+            if not user.authorization == 2:
+                return JsonResponse({'Message': 'unauthorized'}, status = 401)
+
+            data    = json.loads(request.body)
+            title   = data['title']
+            content = data['content']
+
+            AdminBoard.objects.create(
+                user_id = user.id,
+                title   = title,
+                content = content
+            )
+
+            return JsonResponse({'Message': 'SUCCESS'}, status = 200)
+        except KeyError:
+            return JsonResponse({'Message': 'KEY_ERROR'}, status = 400)
+
+class AdminBoardView(View):
+    @login_decorator
+    def get(self, request, adminboard_id):
+        try:
+            user = request.user
+
+            if not user.authorization == 2:
+                return JsonResponse({'Message': 'unauthorized'}, status = 401)
+
+            adminboard = AdminBoard.objects.get(id = adminboard_id)
+            adminboard_detail = {
+                'title'  : adminboard.title,
+                'writer' : adminboard.user.name,
+                'content': adminboard.content
+            }
+
+            return JsonResponse({'results': adminboard_detail}, status = 200)
+        except AdminBoard.DoesNotExist:
+            return JsonResponse({'Message': 'DOES_NOT_EXIST'}, status = 400)  

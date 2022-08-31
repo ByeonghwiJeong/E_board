90e16df6158784f60eacf619a0571bea207e31fd
ByeonghwiJeong <byeonghwijeong@gmail.com>
2022-08-31 23:30:32 +0900

    feat	: 자유게시판 작성(회원가능) & 조회(비회원도가능)

--- board/urls.py
+++ board/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import NoticePostView, NoticeView
+from .views import NoticePostView, NoticeView, AdminBoard, AdminBoardPostView, FreeBoardPostView, FreeBoardView
 
 urlpatterns = [
     path('/notice/post', NoticePostView.as_view()),
     path('/notice/<int:notice_id>', NoticeView.as_view()),
-    path('/adminboard/post', NoticePostView.as_view()),
-    path('/adminboard/<int:adminboard_id>', NoticeView.as_view()),
+    path('/adminboard/post', AdminBoardPostView.as_view()),
+    path('/adminboard/<int:adminboard_id>', AdminBoard.as_view()),
+    path('/freeboard/post', FreeBoardPostView.as_view()),
+    path('/freeboard/<int:freeboard_id>', FreeBoardView.as_view()),
 ]
--- board/views.py
+++ board/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from eboard.utils import login_decorator
 
 from users.models import User
-from .models import AdminBoard, Notice
+from .models import AdminBoard, Notice, FreeBoard
 
 
 class NoticePostView(View):
@@ -88,4 +88,43 @@ class AdminBoardView(View):
 
             return JsonResponse({'results': adminboard_detail}, status = 200)
         except AdminBoard.DoesNotExist:
+            return JsonResponse({'Message': 'DOES_NOT_EXIST'}, status = 400)
+
+
+class FreeBoardPostView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            user = request.user
+
+            if not user.authorization >= 1:
+                return JsonResponse({'Message': 'unauthorized'}, status = 401)
+
+            data    = json.loads(request.body)
+            title   = data['title']
+            content = data['content']
+
+            FreeBoard.objects.create(
+                user_id = user.id,
+                title   = title,
+                content = content
+            )
+
+            return JsonResponse({'Message': 'SUCCESS'}, status = 200)
+        except KeyError:
+            return JsonResponse({'Message': 'KEY_ERROR'}, status = 400)
+
+class FreeBoardView(View):
+    def get(self, request, freeboard_id):
+        try:
+
+            freeboard = FreeBoard.objects.get(id = freeboard_id)
+            freeboard_detail = {
+                'title'  : freeboard.title,
+                'writer' : freeboard.user.name,
+                'content': freeboard.content
+            }
+
+            return JsonResponse({'results': freeboard_detail}, status = 200)
+        except FreeBoard.DoesNotExist:
             return JsonResponse({'Message': 'DOES_NOT_EXIST'}, status = 400)  
